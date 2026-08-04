1106957be7994851ec74f389c9f4bea58b62a4ad
Arya-Bhandari <aryabhandari7989@gmail.com>
2026-08-04 18:38:48 +0530

    build and ulr chnge

--- src/environment/prod.env.ts
+++ src/environment/prod.env.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'https://raya-nestjs.onrender.com/api',
+  baseUrl: 'https://raya-bckend-nestjs.onrender.com/api',
   // baseUrl: 'http://indraprasth.runasp.net',
 };
